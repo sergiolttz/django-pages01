791bcf11dd61eadb5e1b7cae5096654c5fd7e8d4
sergiolttz <sergiolattanzio05@gmail.com>
2024-10-01 18:11:27 -0400

    base

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView
 
-urlpatterns = {
+urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about")
-}
+    path("about/", AboutPageView.as_view(), name="about"),
+]
